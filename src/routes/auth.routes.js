13ad6bbe1e3bcfc272488bdc9ead554a578e175e
hxndlo <hxndlo@hotmail.com>
2021-09-05 22:04:23 -0500

    Editando el router.post en Auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -12,11 +12,7 @@ router.use((req, res, next) => {
   next();
 });
 
-router.post(
-  "/signup",
-  [verifySignup.checkDuplicateUsernameOrEmail, verifySignup.checkRolesExisted],
-  authCtrl.signUp
-);
+
 
 router.post("/signin", authCtrl.signin);
 
